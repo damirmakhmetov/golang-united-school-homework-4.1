75a3e5e859a413d949b744c732ee5060de3b11ef
Damir Makhmetov <damirmakhmetov@gmail.com>
2022-05-26 10:11:04 +0600

    'solution'

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,11 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	runes := []rune(input)
+	var rune_out []rune
+	for i := range runes {
+		rune_out = append(rune_out, runes[len(runes)-i-1])
+	}
+	output = string(rune_out)
 	return output
 }
